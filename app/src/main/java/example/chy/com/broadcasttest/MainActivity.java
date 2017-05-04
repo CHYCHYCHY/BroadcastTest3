47d56d1a09679c29ee16dbcba3ecb286361405e8
CHYCHYCHY <1109418390@qq.com>
2017-05-04 17:00:12 +0800

    Initial commit

---
+++ app/src/main/java/example/chy/com/broadcasttest/MainActivity.java
@@ -0,0 +1,31 @@
+package example.chy.com.broadcasttest;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    class NetworkChangeReceiver extends BroadcastReceiver{
+        @Override
+        public void onReceive(Context context, Intent intent){
+            ConnectivityManager connectivityManager=(ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo=connectivityManager.getActiveNetworkInfo();
+            if(networkInfo !=null&&networkInfo.isAvailable()){
+                Toast.makeText(context,"network is available",Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(context,"network is unavailable",Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
